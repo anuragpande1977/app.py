c5412078e0719636c68f0e98d507767fbca93b7b
anuragpande1977 <137955688+anuragpande1977@users.noreply.github.com>
2024-09-30 00:30:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+from transformers import BartForConditionalGeneration, BartTokenizer
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Load pre-trained BART model and tokenizer
+model_name = "facebook/bart-large-cnn"
+model = BartForConditionalGeneration.from_pretrained(model_name)
+tokenizer = BartTokenizer.from_pretrained(model_name)
+
+# Streamlit app layout
+st.title("Text Summarization with BART")
+
+st.write("Paste your text below to get a summary:")
+
+# Text input from user
+text = st.text_area("Enter the text you want to summarize", height=200)
+
+if st.button("Summarize"):
+    if text:
+        # Tokenize and summarize the input text
+        inputs = tokenizer(text, max_length=1024, return_tensors="pt", truncation=True)
+        summary_ids = model.generate(inputs["input_ids"], num_beams=4, max_length=150, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+        st.write("Summary:")
+        st.write(summary)
+    else:
+        st.write("Please enter some text to summarize.")
